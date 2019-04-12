084b485153a44e2385b33e8bed17b5b9958a464d
Kjoep <jh.cube@gmail.com>
2019-04-12 23:57:52 +0200

    Add docker setup

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.7
+RUN apt-get update
+RUN apt-get install -y curl
+RUN mkdir /data
+WORKDIR /data
+COPY requirements.txt /data
+RUN pip install -r requirements.txt
+COPY setup.py /data/
+COPY certbot_dns_godaddy /data/certbot_dns_godaddy
+RUN pip install -e .
+
+run curl -LO https://storage.googleapis.com/kubernetes-release/release/\
+$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+run chmod +x ./kubectl
+
+ARG GODADDY_KEY
+ARG GODADDY_SECRET
+ARG EMAIL
+ARG DOMAIN
+
+ADD entrypoint.sh .
+CMD ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo certbot_dns_godaddy:dns_godaddy_key=$GODADDY_KEY > godaddy.ini
+echo certbot_dns_godaddy:dns_godaddy_secret=$GODADDY_SECRET >> godaddy.ini
+
+certbot certonly -n -m $EMAIL --agree-tos -d *.$DOMAIN -a certbot-dns-godaddy:dns-godaddy \
+        --certbot-dns-godaddy:dns-godaddy-credentials godaddy.ini
+
+echo "Exposing certs as secrets..."
+
+./kubectl create --dry-run=true -o yaml secret generic wildcard-$DOMAIN \
+    --from-file=/etc/letsencrypt/live/$DOMAIN/cert.pem\
+    --from-file=/etc/letsencrypt/live/$DOMAIN/fullchain.pem\
+    --from-file=/etc/letsencrypt/live/$DOMAIN/chain.pem\
+    --from-file=/etc/letsencrypt/live/$DOMAIN/privkey.pem\
+    > secret.yaml
+
+./kubectl apply -f secret.yaml
